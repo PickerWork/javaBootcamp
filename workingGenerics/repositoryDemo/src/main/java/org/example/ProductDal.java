0fa66df653e8287deec8b37bf76ba8642d72bb3c
PickerWork <pickerwork@gmail.com>
2023-12-06 11:07:32 +0300

    Generics

---
+++ workingGenerics/repositoryDemo/src/main/java/org/example/ProductDal.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class ProductDal implements IEntityRepository<Product>{
+    @Override
+    public void add(Product entity) {
+
+    }
+
+    @Override
+    public void delete(Product entity) {
+
+    }
+
+    @Override
+    public void update(Product entity) {
+
+    }
+
+}
